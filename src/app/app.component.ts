2a6fb0ed62e53b2ba17ff12cf53397cfb8ae59be
IEdiong@Mint <ediongi6@hotmail.com>
2021-09-25 17:44:57 +0100

    Added global styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Hello World</h1>`,
+  template: `
+    <h1>Hello World</h1>
+    <img
+      src="/assets/images/basic-shield.png"
+      alt="angular basic shield image"
+    />
+  `,
 })
 export class AppComponent {
   title: string = 'ng-fundamentals';
