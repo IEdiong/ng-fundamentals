50dc2cfa5de9ddbbbb4820757605357a92b86862
IEdiong@Mint <ediongi6@hotmail.com>
2021-09-26 17:43:02 +0100

    Added my first data-bound component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>Hello World</h1>
-    <img
-      src="/assets/images/basic-shield.png"
-      alt="angular basic shield image"
-    />
-  `,
+  template: ` <events-list></events-list> `,
 })
 export class AppComponent {}
