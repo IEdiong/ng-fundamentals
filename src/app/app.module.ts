429481063f669b32b4089ca3385d9dcc92f5f714
iediong <ediongi6@hotmail.com>
2021-10-04 22:58:14 +0100

    Sending data from parent to child with @Input() decorator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/events-list.component';
 
 @NgModule({
-  declarations: [AppComponent, EventsListComponent],
+  declarations: [AppComponent, EventsListComponent, EventThumbnailComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
