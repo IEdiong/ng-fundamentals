87f618f03e084c9c08eca1323f9a856e493ed3d0
iediong <ediongi6@hotmail.com>
2021-10-13 13:51:11 +0100

    Adding your first route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ToastrService } from './common/toastr.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventService } from './events/shared/event.service';
 import { NavBarComponent } from './nav/nav-bar.component';
+import { appRoutes } from './routes';
 
 @NgModule({
   declarations: [
@@ -14,8 +17,9 @@ import { NavBarComponent } from './nav/nav-bar.component';
     EventsListComponent,
     EventThumbnailComponent,
     NavBarComponent,
+    EventDetailsComponent,
   ],
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [EventService, ToastrService],
   bootstrap: [AppComponent],
 })
