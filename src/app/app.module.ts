a79fcae11b12ccab09de81aed0e3a44f98abf7c0
iediong <ediongi6@hotmail.com>
2021-10-16 23:53:20 +0100

    Guarding against route de-activation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,21 @@ import { appRoutes } from './routes';
     Error404Component,
   ],
   imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
-  providers: [EventService, ToastrService, EventRouteActivator],
+  providers: [
+    EventService,
+    ToastrService,
+    EventRouteActivator,
+    { provide: 'canDeactivateCreateEvent', useValue: 'checkDirtyState' },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+export function checkDirtyState(component: CreateEventComponent) {
+  if (component.isDirty) {
+    return window.confirm(
+      'You have not saved this event. Do you really want to cancel?'
+    );
+  }
+  return true;
+}
